517fb60de067d76faadffe393082a61d1afd569a
Arin Houck <arinhouck@gmail.com>
2018-02-28 22:02:30 -0700

    Comments

--- lib/payment_processor.rb
+++ lib/payment_processor.rb
@@ -3,25 +3,45 @@ class PaymentProcessor
   
   def self.perform
     
+    # Create or find a setting record by `cursor` key
     cursor_setting = Setting.find_or_create_by(key: 'cursor')
+    
+    # Extract the value of the `cursor` if it exists in DB
     cursor = cursor_setting&.value
+    
+    # Instantiate the Stellar SDK by merchant account public key and cursor
     sdk = ::Stellar.new(ENV.fetch('ACCOUNT_ID', 'GCBTFNXKR276VT2O7IP5G7MZFOGMPB4UNFQUOIXC2AKAAD7DYMF23O7N'), cursor)
+    
+    # Instantiate list of memos
     memos = []
     
+    # Extract transactions
     transactions = sdk.transactions
+    
+    # Loop until no transactions left (empty)
     until transactions.empty?
+      
+      # iterate each transaction and extract memo
       transactions.each do |tr|
         memos << tr.memo
+        # Set cursor to call API again (API is paginated)
         sdk.cursor = tr.paging_token
       end
+      
+      # When you call transactions again it will
+      # extract transactions after new cursor
       transactions = sdk.transactions
     end
     
+    # Mark all purchases as completed by memo (not include nil values)
     Purchase.where(memo: memos.compact).update_all(completed: true)
     
+    # Update latest cursor in database
+    # so next time payment processor starts it
+    # will load from latest cursor
     if sdk.cursor != cursor
       cursor_setting.update_column(:value, sdk.cursor)
     end
   end
-  
+
 end
--- lib/sdk/stellar.rb
+++ lib/sdk/stellar.rb
@@ -2,32 +2,34 @@ class Stellar
   include HTTParty
   
   base_uri ENV.fetch('STELLAR_BASE_URI', 'https://horizon-testnet.stellar.org')
-
+  
   attr_accessor :cursor
-
+  
   def initialize(account_id, cursor)
+    # account public key
     @account_id = account_id
+    # how many records to include per call
     @limit = 1
+    # optional value to keep track of where we last processed a transaction
     @cursor = cursor
   end
-
-  def account
-    response = self.class.get("/accounts/#{@account_id}")
-    to_object(response.body)
-  end
   
+  # GET call to retrieve all transactions
+  # https://www.stellar.org/developers/horizon/reference/endpoints/transactions-for-account.html#request
   def transactions
     response = self.class.get("/accounts/#{@account_id}/transactions?limit=#{@limit}#{build_cursor}")
-    to_object(response.body).try(:_embedded).try(:records)
+    to_object(response.body).try(:_embedded).try(:records) || []
   end
   
   
   private
-
+  
+  # Set cursor param when retrieving transactions if exists
   def build_cursor
     @cursor.nil? ? '' : "&cursor=#{@cursor}"
   end
-
+  
+  # Fancy way to parse json into Ruby OpenStruct object
   def to_object(response_body)
     JSON.parse(response_body, object_class: OpenStruct)
   end
